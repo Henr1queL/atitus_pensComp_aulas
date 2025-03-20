af369023602c1f115d084dd6abfc99983498bd44
Henr1queL <1138757@Atitus.edu.br>
2025-03-19 22:09:14 -0300

    aula dia 19/03

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -1,21 +1,17 @@
 def hora_para_minuto(valor):
-    # TODO: preencher
-    pass
-
+    return valor * 60
 
 def minuto_para_segundo(valor):
-    # TODO: preencher
-    pass
-
+   return valor * 60
 
 def hora_para_segundo(valor):
-    # TODO: preencher, deve usar minuto_para_segundo()
-    pass
+    return hora_para_minuto(valor)  *60
 
 
 assert hora_para_minuto(0) == 0
 assert hora_para_minuto(1) == 60
 assert hora_para_minuto(2) == 120
+assert hora_para_minuto(5) == 300
 
 assert minuto_para_segundo(0) == 0
 assert minuto_para_segundo(1) == 60
@@ -23,6 +19,7 @@ assert minuto_para_segundo(2) == 120
 
 assert hora_para_segundo(0) == 0
 assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
+print(hora_para_segundo(2))
 assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
 
 print("Terminou com sucesso!")
