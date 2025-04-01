97a9346d7f2377ff929d41b64fcef0f8244c08cb
Henr1queL <1138757@Atitus.edu.br>
2025-03-31 21:39:02 -0300

    test

--- 04_operadores_logicos_e_condicoes/bissexto.py
+++ 04_operadores_logicos_e_condicoes/bissexto.py
@@ -1,23 +1,15 @@
 def eh_bissexto(ano):
-    resto = ano % 4:
-    if  == 0:
-        return True 
-    else: 
-        return False 
+    if (ano % 4 == 0):
+        return True
+    else:
+        return False
 
-
+#solução professor
 def proximo_bissexto(ano):
-    if ano % 4 == 0:
-        return ano
-    if ano % 4 == 1:
-        return ano + 3
-    if ano % 4 == 2:
-        return ano + 2
-    if ano % 4 == 3:
-        return ano + 1
+    return(ano + 3) // 4 * 4 
 
-def test(): 
 
+def test():
     assert eh_bissexto(0)
     assert eh_bissexto(2020)
     assert eh_bissexto(2024)
@@ -30,10 +22,7 @@ def test():
     assert proximo_bissexto(2025) == 2028
     assert proximo_bissexto(2029) == 2032
     assert proximo_bissexto(2020) == 2020
-
+    
+print(eh_bissexto(0))
 print(eh_bissexto(2020))
-print(eh_bissexto(1907))
-print(eh_bissexto(4560))
-print(proximo_bissexto(2025))
-print(proximo_bissexto(2022))
-print(proximo_bissexto(2020))
+print(eh_bissexto(2024))
