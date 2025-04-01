eba5b877a1e273379a677648dc35e7f4d45b578d
Henr1queL <1138757@Atitus.edu.br>
2025-03-31 21:57:09 -0300

    Fahremheit para Celsius

---
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/Fahrenheit.py
@@ -0,0 +1,6 @@
+temperatura = int(imput ("Digite uma temperatura em Fahrenheit"))
+return celsius = (temperatura - 32) * 5/9
+    return fahrenheit = (temperatura * 9/5) + 32
+
+    print(celsius"C" "e" fharenheit"F")
+
