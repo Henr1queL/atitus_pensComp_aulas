1ec3f5ff0558096762af7eb126c02323796d390e
Henr1queL <1138757@Atitus.edu.br>
2025-03-31 21:29:18 -0300

    media de nota

---
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/media de nota.py
@@ -0,0 +1,8 @@
+a = int(imput("Colouqe a primeira nota"))
+b = int(imput("Coloque a segunda nota"))
+c = int(imput("Colouque a terceira nota"))
+d = int(imput("Colouqe a quarta nota"))
+
+media = (a + b + c + d) / 4
+
+print(media >= 7)
