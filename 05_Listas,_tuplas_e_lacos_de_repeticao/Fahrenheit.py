efdc8c7fbea09475734d04f924d70ff9395b9eac
Henr1queL <1138757@Atitus.edu.br>
2025-04-09 15:13:11 -0300

    test

--- 05_Listas,_tuplas_e_lacos_de_repeticao/Fahrenheit.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/Fahrenheit.py
@@ -1,9 +1,9 @@
-temperatura = float(imput ("Digite uma temperatura:"))
-
-celsius = (temperatura - 32) * 5 / 9
-fahrenheit = (temperatura * 9 / 5) + 32
-
-print(celsius"C e" fahrenheit"F")
+temp_f = int(input('Diga a temperatura em Fahrenheit:'))
+f_para_c = (temp_f - 32)/1.8
+print('A temperatura em graus Celcius é', f_para_c, 'C°')
 
 def test():
 
+temp_c = int(input('Diga a temperatura em Celcius'))
+c_para_f = (1.8* temp_c) + 32
+print('A temperatura em Farenheit é', c_para_f, 'F°'
