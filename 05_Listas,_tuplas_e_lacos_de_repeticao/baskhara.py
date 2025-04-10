91ffe353254e59310d113f3f6eb48bf056897666
Henr1queL <1138757@Atitus.edu.br>
2025-04-10 19:50:07 -0300

    test

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -3,13 +3,13 @@ b = int(input("Digite B"))
 c = int(input("Digite C"))
 
 
-    delta = a ** 2 - 4 * a * c
-    if delta < 0:
-        print("A equação não tem raizes verdadeiras")
-    else:
-        x1 = (-b + delta ** 0.5) /  (2* a)
-        x2 = (-b - delta ** 0.5) /  (2* a)
-        print(f"As raizes são {x1} e {x2}")
+delta = a ** 2 - 4 * a * c
+if delta < 0:
+    print("A equação não tem raizes verdadeiras")
+else:
+     x1 = (-b + delta ** 0.5) /  (2* a)
+    x2 = (-b - delta ** 0.5) /  (2* a)
+print(f"As raizes são {x1} e {x2}")
 
 
 def test():
