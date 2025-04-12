48284206095d2f8b9fc5fa3924028985565760d3
Henr1queL <1138757@Atitus.edu.br>
2025-04-12 20:36:14 -0300

    test

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,12 +1,15 @@
 senha =  5
-tentativas = 1
-descubra_a_senha = int(input("Digite a Senha de 1 a 10"))
-
-while descubra_a_senha != senha:
-    print("Senha Incorreta!")
-    descubra_a_senh = int(input("Digite a senha novamente:"))
-    tentativa += 1
+tentativas = 0
 
 def test():
-    print("Senha Correta!")
-    print("Número de Tentativas:", tentativas)
+ descubra_a_senha = int(input("Digite a Senha de 1 a 10"))
+
+ while descubra_a_senha != senha:
+    print("Senha Incorreta!")
+    descubra_a_senha = int(input("Digite a senha novamente:"))
+    tentativa += 1
+
+
+print("Senha Correta!")
+print(f"Número de Tentativas: {tentativas}")
+
