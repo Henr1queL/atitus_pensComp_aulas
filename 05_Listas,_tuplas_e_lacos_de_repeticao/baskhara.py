316fe0e6868ead31d718cea07eb3845d882aeabb
Henr1queL <1138757@Atitus.edu.br>
2025-04-12 16:38:49 -0300

    t

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -17,7 +17,7 @@ def baskhara(a, b, c):
   
 def test():
 
-    assert baskhara(1, -3, 2) == [2, 1]
+    assert baskhara(1, -3, 2) == [1.0, 2.0]
     assert baskhara(2, 3, -2) == [-2, 0.5]
     assert baskhara(1, -5, 6) == [2, 3]
     assert baskhara(1, -7, 10) == [2, 5]
