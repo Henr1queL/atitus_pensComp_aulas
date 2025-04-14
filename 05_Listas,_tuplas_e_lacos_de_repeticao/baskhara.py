a59c99c725d619a69d3f7186e72414e39a9ed9e3
Henr1queL <1138757@Atitus.edu.br>
2025-04-14 19:11:01 -0300

    test

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -3,7 +3,6 @@ import math
 def baskhara(a, b, c):
      delta = a ** 2 - 4 * a * c
      if delta < 0:
-        print("A equação não tem raizes verdadeiras")
         return None
      else:
         x1 = (-b + math.sqrt(delta)) /  (2 * a)
@@ -16,10 +15,9 @@ def baskhara(a, b, c):
 
   
 def test():
-    assert baskhara(1, -3, 2) == [2, 1]
+   assert baskhara(1, -3, 2) == [2, 1]
     assert baskhara(2, 3, -2) == [-2, 0.5]
     assert baskhara(1, -5, 6) == [2, 3]
     assert baskhara(1, -7, 10) == [2, 5]
-
-assert baskhara(1, 2, 3) is None
-assert baskhara(1, 0, 0) == 0
+    assert baskhara(1, 2, 3) is None
+    assert baskhara(1, 0, 0) == 0
