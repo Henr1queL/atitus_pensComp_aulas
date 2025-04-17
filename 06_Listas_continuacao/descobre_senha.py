d5a599a66ea3416d46e6e790add034ccf34ed3d5
Henr1queL <1138757@Atitus.edu.br>
2025-04-17 15:24:56 -0300

    test

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,11 +1,11 @@
-def qual_senha(senha, tentativas):
-    tentativas = 0
-    for tentativa in tentativas:
+def qual_senha(senha, tentativas_totais):
+    tentativas_totais = 0
+    for tentativa in tentativas_totais:
         if tentativa < 1 or tentativa > 10:
             continue
-        tentativas += 1
+        tentativas_totais += 1
         if tentativa == senha:
-            return tentativas  
+            return tentativas_totais
     return -1  
 
 def test():
