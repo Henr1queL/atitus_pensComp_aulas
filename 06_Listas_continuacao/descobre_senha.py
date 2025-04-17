085348d5728f692371b2a7c14ad17d25906c19b7
Henr1queL <1138757@Atitus.edu.br>
2025-04-17 15:04:47 -0300

    test

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,21 +1,15 @@
-def qual_senha():
-    senha = 5
+def jogo_descobre_senha(senha_secreta, tentativas_usuario):
     tentativas = 0
+    for tentativa in tentativas_usuario:
+        if tentativa < 1 or tentativa > 10:
+            continue
+        tentativas += 1
+        if tentativa == senha_secreta:
+            return tentativas  
+    return -1  
 
-
-descubra_a_senha = int(input("Digite a Senha de 1 a 10"))
-
-while descubra_a_senha != senha:
-    print("Senha Incorreta!")
-    descubra_a_senha = int(input("Digite a senha novamente:"))
-    tentativa += 1
-
-
-print("Senha Correta!")
-print(f"Número de Tentativas: {tentativas}")
-
-def test():
-    assert descobra_a_senha(5, [5]) == 1
-    assert descobra_a_senha(5, [0, 11, 3, 5]) == 2  
-    assert descobra_a_senha(5, [1, 2, 3, 4, 5]) == -1
-    assert descobra_a_senha(5, [1, 2, 3, 4, 5, 5]) == 5
+def test_jogo_descobre_senha():
+    assert jogo_descobre_senha(6, [6]) == 1
+    assert jogo_descobre_senha(6, [0, 11, 3, 6]) == 2  
+    assert jogo_descobre_senha(6, [1, 2, 3, 4, 5]) == -1
+    assert jogo_descobre_senha(6, [1, 2, 3, 4, 5, 6]) == 6
