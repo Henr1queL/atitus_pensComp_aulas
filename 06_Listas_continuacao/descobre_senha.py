8be7dbce9bd607ed714ff0676f556a65f286e24a
Henr1queL <1138757@Atitus.edu.br>
2025-04-17 15:57:59 -0300

    test

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,1 +1,15 @@
+def descobre_senha(senha_secreta, total_tentativas):
+    tentativas = 0
+    for tentativa in total_tentativas:
+        if tentativa < 1 or tentativa > 10:
+            continue
+        tentativas += 1
+        if tentativa == senha_secreta:
+            return tentativas  
+    return -1  
 
+def test_descobre_senha():
+    assert descobre_senha(6, [6]) == 1
+    assert descobre_senha(6, [0, 11, 3, 6]) == 2  
+    assert descobre_senha(6, [1, 2, 3, 4, 5]) == -1
+    assert descobre_senha(6, [1, 2, 3, 4, 5, 6]) == 6
