88eb9f74c199bd6b0192e17868c6edc0f429cf0a
Henr1queL <1138757@Atitus.edu.br>
2025-04-17 15:00:20 -0300

    test

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,27 +1,22 @@
-def descubra_a_senha(senha, tentativas_lista):
+def qual_senha():
+
+    senha = 5
     tentativas = 0
-    for tentativa in tentativas_lista:
-        if tentativa == senha:
-            return tentativas + 1 
-        tentativas += 1
-    return -1  
 
-senha = 5
-tentativas_lista = []
 
-descubra_a_senha_input = int(input("Digite a Senha de 1 a 10: "))
+descubra_a_senha = int(input("Digite a Senha de 1 a 10"))
 
-while descubra_a_senha_input != senha:
+while descubra_a_senha != senha:
     print("Senha Incorreta!")
-    tentativas_lista.append(descubra_a_senha_input)
-    descubra_a_senha_input = int(input("Digite a senha novamente: "))
+    descubra_a_senha = int(input("Digite a senha novamente:"))
+    tentativa += 1
 
-tentativas_lista.append(descubra_a_senha_input)
+
 print("Senha Correta!")
-print(f"Número de Tentativas: {len(tentativas_lista)}")
+print(f"Número de Tentativas: {tentativas}")
 
 def test():
-    assert descubra_a_senha(5, [5]) == 1
-    assert descubra_a_senha(5, [0, 11, 3, 5]) == 2  
-    assert descubra_a_senha(5, [1, 2, 3, 4, 5]) == -1
-    assert descubra_a_senha(5, [1, 2, 3, 4, 5, 5]) == 5
+    assert descobra_a_senha(5, [5]) == 1
+    assert descobra_a_senha(5, [0, 11, 3, 5]) == 2  
+    assert descobra_a_senha(5, [1, 2, 3, 4, 5]) == -1
+    assert descobra_a_senha(5, [1, 2, 3, 4, 5, 5]) == 5
