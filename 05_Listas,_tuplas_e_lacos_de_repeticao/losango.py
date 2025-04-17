2b035de57f051416e0efe1d80176f70ad1c84e1c
Henr1queL <1138757@Atitus.edu.br>
2025-04-17 14:45:19 -0300

    test

--- 05_Listas,_tuplas_e_lacos_de_repeticao/losango.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/losango.py
@@ -1,13 +1,7 @@
-def test():
-    altura = int(input("Digite um número ímpar maior ou igual a 3: "))
+def desenha_losango(altura):
+    # preencher
+    pass
 
-    if altura < 3 or altura % 2 == 0:
-        print("Número inválido. Digite um número ímpar maior ou igual a 3.")
-    else:
-        meio = altura // 2
 
-        for i in range(meio + 1):
-            print(" " * (meio - i) + "*" * (2 * i + 1))
-
-        for i in range(meio - 1, -1, -1):
-            print(" " * (meio - i) + "*" * (2 * i + 1))
+altura = int(input("Digite um valor ímpar para a altura do losango: "))
+desenha_losango(altura)
