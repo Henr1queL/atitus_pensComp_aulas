ae88d4b28c9690161ea9712a7947c7be41d7ed6b
Henr1queL <1138757@Atitus.edu.br>
2025-05-20 19:27:39 -0300

    test

--- 10_Introducao_a_funcoes/calculadora_datas.py
+++ 10_Introducao_a_funcoes/calculadora_datas.py
@@ -1,53 +1,25 @@
-"""
-Meses sao representados como: 1=Jan, 2=Feb, 3=Mar..
-"""
+def advinha_senha(numero_sorteado, palpites):
+    respostas = []
+    tentativas = 3
 
-MESES_31_DIAS = [1, 3, 5, 7, 8, 10, 12]
-MESES_30_DIAS = [4, 6, 9, 11]
+    for tentativa, palpite in enumerate(palpites[:tentativas], 1):
+        if palpite == numero_sorteado:
+            respostas.append(f"Parabéns! Você acertou o número {numero_sorteado} na tentativa {tentativa}.")
+            break
+        elif palpite < numero_sorteado:
+            respostas.append("O número sorteado é MAIOR do que o seu palpite.")
+        else:
+            respostas.append("O número sorteado é MENOR do que o seu palpite.")
+    else:
+        respostas.append(f"Suas tentativas acabaram! O número sorteado era {numero_sorteado}.")
 
+    return respostas  
 
-def eh_bissexto(ano: int) -> bool:
-    # Já feito em aula
-    pass
+def test():
+    numero = 7
+    palpites = [3, 8, 7]
+    resultado = advinha_senha(numero_sorteado, palpites)
 
-
-def total_dias_no_mes(mes: int, ano: int) -> int:
-    # return 28, 29, 30 ou 31
-    pass
-
-
-assert total_dias_no_mes(1, 2024) == 31
-assert total_dias_no_mes(2, 2024) == 29
-assert total_dias_no_mes(3, 2024) == 31
-assert total_dias_no_mes(11, 2024) == 30
-
-
-def formata_data(data: list) -> str:
-    # data = [dia, mes, ano]
-    pass
-
-
-assert formata_data([1, 2, 2024]) == "1/2/2024"
-assert formata_data([1, 12, 2024]) == "1/12/2024"
-
-
-def calcula_diferenca(data1: list, data2: list) -> int:
-    pass
-
-
-# Diferenca em dias entre 2/7/2004 e 27/5/2024 é de 7268 dias
-assert calcula_diferenca([2, 7, 2004], [27, 5, 2024]) == 7268
-# Diferenca entre 27/5/2024 e 2/7/2089 é de 23779 dias
-assert calcula_diferenca([27, 5, 2024], [2, 7, 2004 + 85]) == 23779
-# Diferenca entre 2/7/2004 e 2/7/2089 é de 31047 dias
-assert calcula_diferenca([2, 7, 2004], [2, 7, 2004 + 85]) == 31047
-# A data 27/5/2024 representa 23.409669211195926% entre 2/7/2004 e 2/7/2089
-
-
-# Diferenca em dias entre 24/7/1991 e 24/10/2024 é de 12146 dias
-assert calcula_diferenca([24, 7, 1991], [24, 10, 2024]) == 12146
-# Diferenca entre 24/10/2024 e 24/7/2076 é de 18900 dias
-assert calcula_diferenca([24, 10, 2024], [24, 7, 1991 + 85]) == 18900
-# Diferenca entre 24/7/1991 e 24/7/2076 é de 31046 dias
-assert calcula_diferenca([24, 7, 1991], [24, 7, 1991 + 85]) == 31046
-# A data 24/10/2024 representa 39.12259228241963% entre 24/7/1991 e 24/7/2076
+    assert resultado[0] == "O número sorteado é MAIOR do que o seu palpite."
+    assert resultado[1] == "O número sorteado é MENOR do que o seu palpite."
+    assert resultado[2] 
