95ae1f6540d317b1ce63ec16b831560f0255b336
Henr1queL <1138757@Atitus.edu.br>
2025-05-20 14:28:01 -0300

    test

--- 08_Strings_random/cifra_cesar.py
+++ 08_Strings_random/cifra_cesar.py
@@ -14,10 +14,11 @@ def caesar_cipher(texto, desvio):
     
     return ''.join(resultado)
 
-assert caesar_cipher("Hello, World!", 3) == "Khoor, Zruog!"
-assert caesar_cipher("Khoor, Zruog!", -3) == "Hello, World!"
+def test():
+    assert caesar_cipher("Hello, World!", 3) == "Khoor, Zruog!"
+    assert caesar_cipher("Khoor, Zruog!", -3) == "Hello, World!"
 
-assert caesar_cipher("Matheus Jardim", 3) == "Pdwkhxv Mduglp"
-assert caesar_cipher("Pdwkhxv Mduglp", -3) == "Matheus Jardim"
+    assert caesar_cipher("Matheus Jardim", 3) == "Pdwkhxv Mduglp"
+    assert caesar_cipher("Pdwkhxv Mduglp", -3) == "Matheus Jardim"
 
-assert caesar_cipher(caesar_cipher("Atitus", 3), -3) == "Atitus"
+    assert caesar_cipher(caesar_cipher("Atitus", 3), -3) == "Atitus"
