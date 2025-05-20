92e29209c8dae6f855e59604d7461d8dbea95e1b
Henr1queL <1138757@Atitus.edu.br>
2025-05-20 14:23:41 -0300

    test

--- 08_Strings_random/ordena.py
+++ 08_Strings_random/ordena.py
@@ -1,14 +1,15 @@
 def ordena_lista(lista):
-      n = len(lista)
-
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if lista[j] > lista[j + 1]:
-                lista[j], lista[j + 1] = lista[j + 1], lista[j]
-    return lista  
+    for i in range (len(lista)):
+        for x in range (len(lista)-1):
+            if lista[x] > lista[j+1]:
+                aux = lista[x]
+                lista[x] = lista[x+1]
+                lista[x+1] = aux
+    return lista
+                
 
 def test():
-assert ordena_lista([]) == []
-assert ordena_lista([1]) == [1]
-assert ordena_lista([4, 3, 2, 2, 1, 0]) == [0, 1, 2, 2, 3, 4]
-assert ordena_lista([10, 0, -3, 42, 5, -6, 8, 91]) == [-6, -3, 0, 5, 8, 10, 42, 91]
+    assert ordena_lista([]) == []
+    assert ordena_lista([1]) == [1]
+    assert ordena_lista([4, 3, 2, 2, 1, 0]) == [0, 1, 2, 2, 3, 4]
+    assert ordena_lista([10, 0, -3, 42, 5, -6, 8, 91]) == [-6, -3, 0, 5, 8, 10, 42, 91]
